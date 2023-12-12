b8fb0e882597fc8f07549737ca40b085a6c33d91
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-12-12 17:36:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const isPowerOfTwo = (number) => {
   //your code here
+	if (number <= 0) return false;
+    return (number & (number - 1)) === 0;
 };
-
 const num = parseInt(prompt("Enter a number"));
 alert(isPowerOfTwo(num));
